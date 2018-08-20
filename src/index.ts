a517dcebc19f7d94c8dae3827cd3ca317bebf2ce
Leon Jäkel <info@leon-jaekel.com>
2018-08-20 19:44:52 +0200

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { EventObserver } from './lib/event-observer';
 import { EventObserverFactory } from './lib/event-observer.factory';
+import { EventSubject } from '../dist/lib/event-subject';
 
-export { EventObserver, EventObserverFactory };
+export { EventObserver, EventObserverFactory, EventSubject };
